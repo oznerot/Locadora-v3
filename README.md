254bbbfb608e8b1e1686fe72d2956331136d3c79
oznerot <vitorenzocs@gmail.com>
2024-08-31 17:35:56 -0300

    Inicializacao do projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# Locadora-v3
---
+++ src/main/java/br/ufscar/dc/dsw/LocadoraV3Application.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraV3Application.class, args);
+	}
+
+}
