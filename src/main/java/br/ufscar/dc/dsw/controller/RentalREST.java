72de660ee25a08497528fd325fe4999d18b83197
oznerot <vitorenzocs@gmail.com>
2024-08-31 20:17:35 -0300

    a

---
+++ src/main/java/br/ufscar/dc/dsw/controller/RentalREST.java
@@ -0,0 +1,26 @@
+package br.ufscar.dc.dsw.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.ufscar.dc.dsw.domain.Client;
+import br.ufscar.dc.dsw.service.spec.IClientService;
+import jakarta.validation.Valid;
+
+@RestController
+public RentalREST
+{
+
+}
